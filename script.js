0241cc7e3dfca742a3e3f251bf43c2188394091e
Richardrflsn <105922097+Richardrflsn@users.noreply.github.com>
2023-02-26 16:01:04 +0700

    popup working

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-var loginForm = document.getElementById("loginForm");
-var registerForm = document.getElementById("registerForm");
-var indicator = document.getElementById("indicator");
-
-function register(){
-    registerForm.style.transform = "translateX(0px)";
-    loginForm.style.transform = "translateX(0px)";
-    indicator.style.transform = "translateX(100px)";
-}
-function login(){
-    registerForm.style.transform = "translateX(300px)";
-    loginForm.style.transform = "translateX(300px)";
-    indicator.style.transform = "translateX(0px)";
-}
-
 // popup login
 document.querySelector("#show-login").addEventListener("click", function(){
     document.querySelector(".popup").classList.add("active");
