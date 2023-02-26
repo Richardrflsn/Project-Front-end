9f9eb5ffaa3483d1bf788d03f1fb0bf4a2709f07
Richardrflsn <105922097+Richardrflsn@users.noreply.github.com>
2023-02-26 15:36:01 +0700

    popup login (not fix :) )

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function login(){
     loginForm.style.transform = "translateX(300px)";
     indicator.style.transform = "translateX(0px)";
 }
+
+// popup login
+document.querySelector("#show-login").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+});
